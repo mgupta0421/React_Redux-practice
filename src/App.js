72c1764586e16184df45711f959c9a9af4853cf6
Mitali <mitaligupta726@gmail.com>
2020-03-11 23:50:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './App.css';
+import UserOutput from './UserOutput/UserOutput' 
+import UserInput from './UserInput/UserInput' 
+
+class App extends Component{
+   state = {
+     username: "rajat"
+   }
+
+  eventHandler =(event) =>{
+    this.setState({username: event.target.value});
+
+   } 
+  render() {
+   return (
+    <div>
+      
+      <UserInput changed = {this.eventHandler}/>
+      <UserOutput  username = {this.state.username}/>
+      <UserOutput username= {this.state.username}/>
+    </div>
+   );
+  }
+}
+
+export default App;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const userOutput = (props) => {
+	return (
+		<div>
+		<p>Username: {props.username}</p>
+		<p>my age is 92</p>
+		</div>
+	);
+
+};
+
+export default userOutput;
